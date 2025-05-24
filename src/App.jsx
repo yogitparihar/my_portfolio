df52dd6e6a8d5f4c8f3193f615b60812cae9b89b
yogit parihar <yogit.parihar@sofmen.com>
2025-05-24 17:08:53 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import Navbar from "./components/Navbar";
